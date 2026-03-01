f86546d3238c3f69d72cf50482544f3f217a570b
YoniPel <yehonatan3000@gmail.com>
2026-03-01 02:50:58 +0200

    update main to enable the user to crack more than one password

--- src/main.cpp
+++ src/main.cpp
@@ -16,50 +16,54 @@ int main()
 3. That's it :) 
     )" << std::endl; 
     
-    std::cout << "Enter the number of thread you want to use: "; 
 
-    std::getline(std::cin, numOfThreadsString);
+    while(true) {
+         std::cout << "Enter the number of thread you want to use: "; 
 
-    int numOfThreads = 1; 
-    try {
-        numOfThreads = std::stoi(numOfThreadsString);
+        std::getline(std::cin, numOfThreadsString);
 
-        int maxNumOfThreads = std::thread::hardware_concurrency();
-        if (numOfThreads < 1 || numOfThreads > maxNumOfThreads) {
-            std::cout << "Invalid number, the number should be between 1 to " << maxNumOfThreads << std::endl; 
+        int numOfThreads = 1; 
+        try {
+            numOfThreads = std::stoi(numOfThreadsString);
+
+            int maxNumOfThreads = std::thread::hardware_concurrency();
+            if (numOfThreads < 1 || numOfThreads > maxNumOfThreads) {
+                std::cout << "Invalid number, the number should be between 1 to " << maxNumOfThreads << std::endl; 
+                return 1; 
+            }
+        } catch (const std::exception& e) {
+            std::cout << "That's not a valid number" << std::endl; 
             return 1; 
         }
-    } catch (const std::exception& e) {
-        std::cout << "That's not a valid number" << std::endl; 
-        return 1; 
+
+        std::string secret; 
+        
+        std::cout << "Enter a numeric password between 0 to 1,000,000,000: "; 
+
+        std::getline(std::cin, secret);
+
+        if (secret.empty()) {
+            std::cout << "You didn't enter anything" << std::endl; 
+            return 1; 
+        }
+
+
+        PasswordCracker cracker(numOfThreads);
+
+        std::cout << "Cracking password with " << numOfThreads << " threads" << std::endl; 
+
+        auto start = std::chrono::high_resolution_clock::now();
+
+        int result = cracker.crackPassword(std::stoi(secret));
+
+        auto end = std::chrono::high_resolution_clock::now();
+
+        std::chrono::duration<double> elapsed = end - start;
+
+        std::cout << "The password is: " << result << std::endl; 
+        std::cout << "Time taken: " << elapsed.count() << " seconds\n" << std::endl;  
     }
+   
 
-    std::string secret; 
-    
-    std::cout << "Enter a numeric password between 0 to 1,000,000,000: "; 
-
-    std::getline(std::cin, secret);
-
-    if (secret.empty()) {
-        std::cout << "You didn't enter anything" << std::endl; 
-        return 1; 
-    }
-
-
-    PasswordCracker cracker(std::stoi(secret), numOfThreads);
-
-    std::cout << "Cracking password with " << numOfThreads << " threads" << std::endl; 
-
-    auto start = std::chrono::high_resolution_clock::now();
-
-    int result = cracker.crackPassword();
-
-    auto end = std::chrono::high_resolution_clock::now();
-
-    std::chrono::duration<double> elapsed = end - start;
-
-    std::cout << "The password is: " << result << std::endl; 
-    std::cout << "Time taken: " << elapsed.count() << " seconds" << std::endl; 
- 
     return 0;
 }
